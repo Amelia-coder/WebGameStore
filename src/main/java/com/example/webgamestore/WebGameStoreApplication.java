bc8f43fe76f15f36d4b1fbb300dbec77656ec2f4
Amelia-coder <o24697692@gmail.com>
2025-06-07 21:38:46 +0300

    Adds gitignore

---
+++ src/main/java/com/example/webgamestore/WebGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.webgamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGameStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebGameStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webgamestore/WebGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webgamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGameStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
